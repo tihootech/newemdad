2c561e117e077e771216fe03bda72529cf5006e2
seyfi68 <ali.seyfi68@gmail.com>
2020-10-30 10:59:27 +0330

    contactus blanked

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -17,9 +17,9 @@
                                 شماره تماس
                             </div>
                             <div class="card-body">
-                                <a href="tel:08338380065"> 08338380065 </a>
+                                <a href="tel:xxxxx"> xxxxx </a>
                                 و
-                                <a href="tel:08338380067"> 08338380067 </a>
+                                <a href="tel:xxxxx"> xxxxx </a>
                             </div>
                         </div>
                     </div>
@@ -29,14 +29,14 @@
                                 آدرس
                             </div>
                             <div class="card-body">
-                                کرمانشاه – میدان سپاه پاسداران (نفت) – بلوار زن – بلوار بهزیستی – ساختمان شهید سردار قاسم سلیمانی – معاونت اشتغال و کارآفرینی
+                                آدرس - آدرس - آدرس - آدرس -آدرس - آدرس -آدرس - آدرس
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6781.473739169777!2d47.09531001598906!3d34.34515361217466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ffaed777835b5d3%3A0x52130fd66e71a735!2sBehzisti%2C%20Kermanshah%2C%20Kermanshah%20Province!5e0!3m2!1sen!2s!4v1591718648489!5m2!1sen!2s" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210849.41019470428!2d46.955788610634634!3d34.33718983796068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ffaed9fb779436f%3A0xdfd40ef2ccc68ad3!2sKermanshah%2C%20Kermanshah%20Province!5e0!3m2!1sen!2s!4v1604042913274!5m2!1sen!2s" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
         </div>
     </div>
